b9b0d9e89e0f8ae18638eb81034b1a5aaf9d7e8f
Tropic222 <deputat1120@gmail.com>
2024-08-07 15:50:17 +0300

    designed the left side of the footer

--- src/components/ui/Footer/Footer.jsx
+++ src/components/ui/Footer/Footer.jsx
@@ -1,5 +1,16 @@
+import { Stack, Typography } from '@mui/material';
 import React from 'react';
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <Stack component="footer" sx={{ paddingTop: 4, paddingBottom: 4 }}>
+      <Typography>
+        &copy; {new Date().getFullYear()} &laquo;Divan&raquo; 18+ <br />
+        Данный сайт создан исключительно в обучающих целях.
+        <br />
+        Все права принадлежат правооблодателям.
+      </Typography>
+      <Typography>Divan</Typography>
+    </Stack>
+  );
 }
