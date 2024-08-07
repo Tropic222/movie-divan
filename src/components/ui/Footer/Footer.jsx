38560b85602c5201fd1bd9f3736eced7293ab27b
Tropic222 <deputat1120@gmail.com>
2024-08-07 16:30:29 +0300

    finished the footer

--- src/components/ui/Footer/Footer.jsx
+++ src/components/ui/Footer/Footer.jsx
@@ -3,14 +3,26 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <Stack component="footer" sx={{ paddingTop: 4, paddingBottom: 4 }}>
+    <Stack
+      component="footer"
+      sx={{
+        paddingTop: 4,
+        paddingBottom: 4,
+        flexDirection: { sm: 'row' },
+        justifyContent: { sm: 'space-between' },
+        alignItems: { sm: 'center' },
+        marginTop: 'auto',
+      }}
+    >
       <Typography variant="body2" color="text.secondary">
         &copy; {new Date().getFullYear()} &laquo;Divan&raquo; 18+ <br />
         Данный сайт создан исключительно в обучающих целях.
         <br />
         Все права принадлежат правооблодателям.
       </Typography>
-      <Typography color="primary.main">Divan</Typography>
+      <Typography variant="h4" color="primary.main">
+        Divan
+      </Typography>
     </Stack>
   );
 }
