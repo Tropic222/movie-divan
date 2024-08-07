da87784ff341bbd03a17bf8fed8805d03fd08daf
Tropic222 <deputat1120@gmail.com>
2024-08-07 15:53:29 +0300

    added color footer

--- src/components/ui/Footer/Footer.jsx
+++ src/components/ui/Footer/Footer.jsx
@@ -4,13 +4,13 @@ import React from 'react';
 export default function Footer() {
   return (
     <Stack component="footer" sx={{ paddingTop: 4, paddingBottom: 4 }}>
-      <Typography>
+      <Typography variant="body2" color="text.secondary">
         &copy; {new Date().getFullYear()} &laquo;Divan&raquo; 18+ <br />
         Данный сайт создан исключительно в обучающих целях.
         <br />
         Все права принадлежат правооблодателям.
       </Typography>
-      <Typography>Divan</Typography>
+      <Typography color="primary.main">Divan</Typography>
     </Stack>
   );
 }
