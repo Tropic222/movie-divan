f22f287e2ec3b202a222aa5e9d09f6beb82033e1
Tropic222 <deputat1120@gmail.com>
2024-08-08 19:43:49 +0300

    loading from constants.js and output to console

--- src/components/pages/MoviesListTop/MoviesListTop.jsx
+++ src/components/pages/MoviesListTop/MoviesListTop.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useLocation } from 'react-router-dom';
 
+import { TOP_LISTS } from '../../../constants.js';
 import { useGetFilmsTopQuery } from '../../../services/kinopoiskApi';
 
 export default function MoviesListTop() {
+  const location = useLocation();
+  const [page, setPage] = useState(1);
+  const movieType = TOP_LISTS.find(el => el.url === location.pathname);
   const { data, error, isLoading } = useGetFilmsTopQuery({
-    type: 'TOP_POPULAR_ALL',
-    page: 1,
+    type: movieType.value,
+    page,
   });
   console.log(data, error, isLoading);
   return <div>MoviesListTop</div>;
--- src/constants.js
+++ src/constants.js
@@ -31,42 +31,56 @@ export const TOP_LISTS = [
     title: 'Топ 100 популярных фильмов',
     icon: 'AutoAwesome',
     url: '/popular',
+    value: 'TOP_POPULAR_MOVIES',
   },
   {
     title: 'Топ 250 лучших фильмов',
     icon: 'StarPurple500',
     url: '/best',
+    value: 'TOP_250_MOVIES',
   },
   {
     title: 'Вампиры',
     icon: 'Bloodtype',
     url: '/vampire',
+    value: 'VAMPIRE_THEME',
   },
 
   {
     title: 'Комиксы',
     icon: 'MenuBook',
     url: '/comics',
+    value: 'COMICS_THEME',
   },
   {
     title: 'Семейные',
     icon: 'FamilyRestroom',
     url: '/family',
+    value: 'FAMILY',
+  },
+  {
+    title: 'Романтика',
+    icon: 'VolunteerActivism',
+    url: '/romantic',
+    value: 'LOVE_THEME',
   },
   {
     title: 'Зомби',
     icon: 'MoodBad',
-    url: 'zombie',
+    url: '/zombie',
+    value: 'ZOMBIE_THEME',
   },
   {
     title: 'Катастрофы',
     icon: 'Pool',
-    url: 'catastrophe',
+    url: '/catastrophe',
+    value: 'CATASTROPHE_THEME',
   },
   {
     title: 'Популярные сериалы',
     icon: 'LiveTv',
     url: '/popular-serials',
+    value: 'POPULAR_SERIES',
   },
 ];
 
